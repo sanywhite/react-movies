87f07beeecbf3fcfe3f187e20daf0fc8efaab115
sanywhite <vasilishin.aleks@gmail.com>
2021-05-10 11:04:53 +0300

    added app in repo

---
+++ src/components/Preloader.jsx
@@ -0,0 +1,7 @@
+function Preloader() {
+    return   <div class="progress">
+    <div className="indeterminate"></div>
+</div>
+}
+
+export {Preloader}
---
+++ src/layout/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return <footer className="page-footer blue-grey darken-1">
+    <div className="footer-copyright">
+      <div className="container">
+        
+      © {new Date().getFullYear} Copyright Text
+      <a href="#!">Repo</a>
+      </div>
+    </div>
+  </footer>
+}
+
+export {Footer}
